286b43811a003962ea2d6430c414bb80b87bad0b
abcohmy <abcohmy@gmail.com>
2025-06-20 07:35:01 +0800

    sequelize版的db

--- db/index.js
+++ db/index.js
@@ -1,32 +1,52 @@
-const mysql = require('mysql2/promise'); // promise for async/await 
 
-const dbConfig = {
-    host: process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0    
-}
+const mysql = require('mysql2/promise'); // promise for async/await 
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306,
+        logging: msg =>{
+            //只輸出SQL與據
+            if (msg.startsWith('Executing')){
+                console.log(`[SQL] ${new Date().toISOString()} - ${msg}`);
+            }
+        },
+        pool: {
+            // 最多連結數
+            max: 5,
+            //最少空閒數
+            min: 0,
+            //獲取連結的最大等待時間(毫秒)
+            acquire: 60000
+            //閒置狀態最大時間(毫秒)
+            idle:60000
+        },
+        //舊版本MYSQL需要
+        dialectOptions: {},
+        timezone: '+08:00' // 台灣時區
+    }
+);
 //pool只希望被建立一次, 建一次要很久
 let pool;
 
 async function initializeDb() {
-    if (!pool){
-        //建連線池
-        pool = mysql.createPool(dbConfig);
-        console.log('資料庫連線池已建立');
-    }
-    // 嘗試連接
     try {
-        const connection = await pool.getConnection();
-        connection.release();
+        //嘗試連線, 會自動斷掉
+        await sequelize.authenticate();
+        // 生產環境中須考慮: migrations
+        // 小專案可用 sync({force:true}) => 每次啟動會刪除並重建所有表
+        
+        await sequelize.sync();
+        console.log('所有Sequelize模型已同步。')
         return true;
     } catch (err) {
         console.error ('無法建立資料庫連線:', err);
-        throw err;
+        process.exit(1); //直接離開 相對throw error少了些處理
     }
     
 
@@ -53,4 +73,6 @@ module.exports = {
     initializeDb, 
     query
 };
+
+
 
