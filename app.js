0b8e3c7cee42f188631049d72dd459358bf47030
abcohmy <abcohmy@gmail.com>
2025-06-23 14:40:19 +0800

    改成新版可用

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
 // 載入.env 隱藏的變數資料
 require('dotenv').config();
+const port = process.env.PORT || 5000;
+
 
 const express = require('express');
 const cors = require('cors');
+
+const {initializeDb} = require('./db/index');
+const User = require('./models/userModel');
+const Booking = require('./models/bookingModel');
+const authRoutes = require('./routes/authRoutes');
+const bookingsRoutes = require('./routes/bookingsRoutes');
+
+
+
 const app = express();
-const port = process.env.PORT || 5000;
-
-const db = require('./db');
-const bookingsRoutes = require('./routes/bookings');
-
 //cors middleware網路安全使用
 app.use(cors());
-
-
 //解析json需要的body-parser
 app.use(express.json()); //解析json
-app.use(express.urlencoded({extended:true})); //解析url-encoded 格式的請求體 (HTM 表單提交)
+//app.use(express.urlencoded({extended:true})); //解析url-encoded 格式的請求體 (HTM 表單提交)
 
-
-db.initializeDb()
-    .then(() => console.log('成功連接至資料庫'))
-    .catch(err => console.error('資料庫連線失敗:', err));
-
-app.use('/bookings', bookingsRoutes);
-
+app.use('/api/auth', authRoutes);
+app.use('/api/bookings', bookingsRoutes);
 
 app.get('/', (req, res) => {
     res.send('歡迎來到主頁!');
 });
 
 
-
-
-app.listen(port, () => {
+const startServer = async () => {
+    await initializeDb();
+    app.listen(port, () => {
     console.log (`Express server is listening http://localhost:${port}`);
 });
+};
 
 
-/*
-    TEST
-*/
+startServer();
 
 
-// 1232123
+
+
+
+
+
